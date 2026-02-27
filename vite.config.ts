eda84cc844debe0f7c9aeeaea3e42eae25e56174
Mark Mansfield <markgmansfield@gmail.com>
2026-02-27 12:53:19 +1100

    add shadcn +  MCP server

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
 import relay from 'vite-plugin-relay';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: { '@': path.resolve(__dirname, './src') },
+  },
   plugins: [
     relay,
     react({
